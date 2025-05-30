b6e48bb25b9ac692e9973930bb090f881c21e55d
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-05-30 19:01:48 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ function App() {
       // Você precisa substituir "SUA_URL_DO_BACKEND_NO_RENDER_AQUI" pela URL real do seu serviço no Render.
       // Exemplo: https://seu-projeto-backend.onrender.com/generate-post
      //const response = await fetch("http://localhost:5000/generate-post", {
-      const response = await fetch("https://exames.onrender.com/generate-post", {
+      const response = await fetch("https://exames.onrender.com", {
         method: "POST",
         body: formData, // Envie o FormData
       })
