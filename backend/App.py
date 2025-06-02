5c6030ec91759e269b24b946783d74a566a522d3
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-06-02 10:36:15 -0300

    alt

--- backend/App.py
+++ backend/App.py
@@ -28,13 +28,18 @@ def home():
 
 # ... (Suas outras rotas, como @app.route('/generate-post', methods=['POST'])) ...
 
-CORS(app, resources={r"/*": {"origins": "https://luizcarlosfs.github.io"
+#CORS(app, resources={r"/*": {"origins": "https://luizcarlosfs.github.io"
 }})
 
 # CORS(app, resources={r"/*": {"origins": [
 #     "http://localhost:3000",
 #     "https://luizcarlosfs.github.io"
 # ]}})
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:3000",
+    "https://luizcarlosfs.github.io",
+    "*" # Adicione o curinga aqui para permitir todas as outras origens
+]}})
 
 load_dotenv() # Carrega as variáveis de ambiente do arquivo .env
 
