f0fb05408d67a95fa398895f02058a9f8402c356
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-06-03 09:53:35 -0300

    alt

--- backend/App.py
+++ backend/App.py
@@ -9,7 +9,6 @@ import warnings
 import PyPDF2  
 from dotenv import load_dotenv 
 
-
 warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
@@ -25,13 +24,13 @@ def home():
 
 # CORS(app)
 
-CORS(app, resources={r"/*": {"origins": [
-    "http://localhost:3000",
-    "https://luizcarlosfs.github.io"  # Origem correta
-]}})
+# CORS(app, resources={r"/*": {"origins": [
+#     "http://localhost:3000",
+#     "https://luizcarlosfs.github.io"  # Origem correta
+# ]}})
 
 
-#CORS(app, origins='*') 
+CORS(app, origins='*') 
 
 load_dotenv() # Carrega as variáveis de ambiente do arquivo .env
 
