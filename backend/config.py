4e00f2d893082e59f3568f236e6e9bd144c1d071
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-06-10 16:18:02 -0300

    alteracao 10-06

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,3 @@
 CORS_HEADERS = 'Content-Type, Authorization, Origin, x-csrf-token' 
 CORS_METHODS = 'GET, HEAD, POST, PATCH, DELETE, OPTIONS' 
-CORS_ORIGINS = ['https://luizcarlosfs.github.io', 'http://localhost:3000','http://127.0.0.1:10000']
-
+CORS_ORIGINS = ['https://luizcarlosfs.github.io', 'http://localhost:3000','http://127.0.0.0:10000']
