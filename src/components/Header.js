924cb8119cbd88a79463bacfec632f7bd3966849
LuizCarlosfs <luizcarlosfs58@gmail.com>
2025-06-10 16:02:09 -0300

    acerto

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = () => {
   return (
     <header className='App-header'>
-      <h1>Análise Superficial de Exames</h1>
+      <h1>Análise de Exames</h1>
     </header>
   )
 }
